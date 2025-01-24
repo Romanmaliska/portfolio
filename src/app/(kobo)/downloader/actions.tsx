28720f5b54d8927f240139e78b7d4f7a8a6b50c1
Roman Maliska <roman.maliska@firma.seznam.cz>
2025-01-24 09:44:15 +0100

    fix: return type

--- src/app/(kobo)/downloader/actions.tsx
+++ src/app/(kobo)/downloader/actions.tsx
@@ -4,7 +4,7 @@ const baseUrl = 'https://www.googleapis.com/drive/v3/files';
 
 export const getBooks = async () => {
   if (!process.env.GOOGLE_DRIVE_API_KEY || !process.env.GOOGLE_DRIVE_FILE_ID)
-    return [];
+    return null;
 
   const url = `${baseUrl}/?q=${process.env.GOOGLE_DRIVE_FILE_ID}&key=${process.env.GOOGLE_DRIVE_API_KEY}`;
   const books = await fetch(url, { cache: 'no-store' });
