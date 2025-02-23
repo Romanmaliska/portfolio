cfad1778953677bce33582d1a1a1d5fc3f02f2a5
Roman Maliska <roman.maliska@firma.seznam.cz>
2025-02-23 13:06:46 +0100

    feat: analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from "@vercel/analytics/react"
 import { ThemeProvider } from './ThemeProvider';
 
 import './globals.css';
@@ -35,6 +36,7 @@ export default function RootLayout({
           <main className="p-12 lg:p-24">{children}</main>
           <Socialbar />
         </ThemeProvider>
+        <Analytics />
         <SpeedInsights />
       </body>
     </html>
